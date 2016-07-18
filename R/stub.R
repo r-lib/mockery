737517311e948d9ee7d66711b6f0967c5f95775b
noam finkelstein <noam.finkelstein@gmail.com>
2016-07-18 16:21:17 -0400

    allow to stub out with functions

--- R/stub.R
+++ R/stub.R
@@ -1,9 +1,15 @@
-stub = function(func, to_stub, return_value) {
+stub = function(func, to_stub, stub) {
     original_func = get(func, envir=parent.frame(), mode='function')
     env = environment(original_func)
-    assign(to_stub, function(...) return(return_value), env)
+
+    if (mode(stub) == 'function') {
+        assign(to_stub, stub, env)
+    } else {
+        assign(to_stub, function(...) return(stub), env)
+    }
 
     function_text = get_function_source(original_func)
     new_function = eval(parse(text=function_text), env)
     assign(func, new_function, parent.frame())
 }
+
--- tests/testthat/test_stub.R
+++ tests/testthat/test_stub.R
@@ -4,18 +4,33 @@ f = function(x) x
 test_that('stubs function with return value', {
     # given
     a = 10
-    g = function(x) f(x) + 10
+    g = function(x) f(x) + a
 
     # before stubbing
-    expect_equal(g(10), 20)
+    expect_equal(g(20), 30)
 
     # when
     stub('g', 'f', 100)
 
     # then
-    expect_equal(g(10), 110)
+    expect_equal(g(20), 110)
 })
 
 test_that('values restored after test', {
     expect_equal(f(15), 15)
+})
+
+test_that('stubs function with function', {
+    # given
+    a = 10
+    g = function(x) f(x) + a
+
+    # before stubbing
+    expect_equal(g(20), 30)
+
+    # when
+    stub('g', 'f', function(...) 500)
+
+    # then
+    expect_equal(g(10), 510)
 })
