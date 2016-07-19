b2dd3ca5650cb09efae51e6d6b08031d1bff24df
noam finkelstein <noam.finkelstein@gmail.com>
2016-07-19 13:30:46 -0400

    add partial call list matching

--- R/expect_called.R
+++ R/expect_called.R
@@ -7,8 +7,9 @@ expect_called = function(func, to_mock, expected_call_list) {
     mock_func = get(to_mock, original_env)
     mock_env = restorepoint::clone.environment(environment(mock_func))
 
-    expect_call_equal = function() {
-        call_list = lapply(names(expected_call_list), 
+    expect_call_equal = function(call_list) {
+        call_list = call_list[names(expected_call_list)]
+        call_list = lapply(names(call_list),
                            function(x) eval(parse(text=x), parent.frame(3)))
         names(call_list) = names(expected_call_list)
         testthat::expect_equal(call_list, expected_call_list)
@@ -17,7 +18,7 @@ expect_called = function(func, to_mock, expected_call_list) {
     assign('expect_call_equal', expect_call_equal, mock_env)
 
     function_source = get_function_source(mock_func)
-    function_source = add_first_statement(function_source, 'expect_call_equal()')
+    function_source = add_first_statement(function_source, 'expect_call_equal(as.list(match.call()))')
 
     mocked_function = eval(parse(text=function_source), mock_env)
     assign(to_mock, mocked_function, original_env)
--- tests/testthat/test_expect_called.R
+++ tests/testthat/test_expect_called.R
@@ -11,3 +11,26 @@ test_that('expect called verifies call list', {
     # when
     f(5)
 })
+
+test_that('expect called verifies with partial call list', {
+    # given
+    g = function(y, z, x) y + z + x + 10
+    f = function(x) g(y=1, z=2, x=(x + 1))
+
+    # expect
+    mockery::expect_called('f', 'g', list(y=1, x=6))
+
+    # when
+    f(5)
+})
+
+test_that('expect called works with namespaced funtions', {
+    # given
+    g = function(x, y) testthat::expect_equal(x, y)
+
+    # expect
+    mockery::expect_called('g', 'testthat::expect_equal', list(1, 1))
+
+    # when
+    g(1, 1)
+})
