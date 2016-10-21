55894f64fa9c810e6b0af0a55c56cb5af8e2da17
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-10-20 23:14:25 -0700

    extend docs, fix formatting

--- R/function_source_utils.R
+++ R/function_source_utils.R
@@ -6,26 +6,30 @@
 #'
 #' @export
 #'
-get_function_source = function(func) {
-    output = capture.output(print(func))
+get_function_source <- function(func)
+{
+    output <- capture.output(print(func))
 
     # drops environment and namespace information
-    output = Filter(function(x) !startsWith(trimws(x), '<'), output)
+    output <- Filter(function(x) !startsWith(trimws(x), '<'), output)
 
-    output = add_brackets(output)
-    output = sapply(output, trimws)
-    function_source = paste(output, sep='\n', set='', collapse='')
-    return(function_source)
+    output <- add_brackets(output)
+    output <- sapply(output, trimws)
+    paste(output, '', sep = '\n', collapse = '')
 }
 
-add_first_statement = function(function_source, statement) {
-    return(sub('\\{', paste('{\n', statement, '\n'), function_source))
+
+add_first_statement <- function(function_source, statement)
+{
+    sub('\\{', paste('{\n', statement, '\n'), function_source)
 }
 
-add_brackets = function(output) {
+
+add_brackets <- function(output)
+{
     if (length(output) == 1) {
-        last = max(gregexpr(')', output[1])[[1]])
-        output = c(substr(output, 1, last), '{', substring(output, last + 1), '}')
+        last <- max(gregexpr(')', output[1])[[1]])
+        output <- c(substr(output, 1, last), '{', substring(output, last + 1), '}')
     }
     return(output)
 }
